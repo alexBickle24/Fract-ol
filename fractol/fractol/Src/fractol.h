c9daf71632f5fbb3691aff4197baa7b2b859a022
alex <alexcarrillomc@outlook.com>
2025-03-27 01:58:10 +0100

    cambios antes de crear la rama para guardar la estructura de ventanas, para poder tener varias

--- fractol/fractol/Src/fractol.h
+++ fractol/fractol/Src/fractol.h
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 06:03:56 by alex              #+#    #+#             */
-/*   Updated: 2025/03/07 20:56:21 by alex             ###   ########.fr       */
+/*   Updated: 2025/03/27 01:26:42 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 
 #define WIDTH 800
 #define HEIGHT 600
+
 #define MAND_MAX_X 2
 #define MAND_MIN_X -2
 #define MAND_MAX_Y 2
@@ -29,13 +30,13 @@
 
 ///////////////////////////***Structs***///////////////////////////
 
-typedef struct	s_mlx_utils
+typedef struct	s_mlx_enviroment
 {
-	void				*mlx_data;//lo que devuelve mlx.init
+	void				*mlx_var;//lo que devuelve mlx.init
 	void				*windows;//lo que devuelve 
 	t_image_data		*img_data;
 	t_window_data		*win_data;
-}				t_mlx_utils;
+}				t_mlx_enviroment;
 
 typedef struct	s_window_data//se podrian usar tambien defines y flags para modificarlo abra que destruit y abrir ventanas
 {
@@ -50,33 +51,62 @@ typedef struct	s_image_data
 	char	*bit_map_address;//lo que devuelve mlx_get_data_addr
 	int		bits_per_pixel;//para saber cuantos bits hay en un pixel
 	int		line_length;//para saber cuantos bytes hay en una fila de la imagen
-	int		endian;//se puede modificar en un sentido u otro
-	int		(*set_def)(t_mlx_utils *, t_image_data, int, int);
-	char	**config;
-	int		max_iter;//para la precision de los fractales
+	int		endian;//se puede modificar en un sentido u otro (big endian vs little endian)
+	void	(*set_def)(t_mlx_enviroment *, t_image_data, int, int);//el puntero a la funcion que define el conjunto que se va a renderizar
+	char	**config;//los argumentos de ese conjuntos para hacer variaciones desde la ejecucion
+	int		max_iter;//para la precision de los fractales (mas iteraciones == a mas precision en el calculo de colores)
 	int		color;//todavia no se como configurarlo la idea es mandar los colores desde los argumentos para que funcioe de maneras diferentes
 	double	zoom;//para establecer los valores de zoom desde el argumento del programa
 	int		origin[2];//para establecer el origen de la imagen desde el argumento del programa
 	int		with;//para establece
 }				t_image_data;
 
-typedef struct	s_complex_data
+typedef struct	s_complex_factors
 {
 	double		a;//parte real
 	double		b;//parte imaginaria
-}				t_complex_data;
+}	t_complex_factors;
 
-typedef struct	s_set_select
-{
-	char	*name;
-	int		argz;
-	char	**args;
-	int		(*f)(t_mlx_utils *, t_image_data, int, int);
-	t_set_select	*next_set;
-}				t_set_select;
+
+//si puediese usar globales tendria sentido
+// typedef struct	s_set_select
+// {
+// 	char	*name;
+// 	int		argz;
+// 	char	**args;
+// 	int		(*f)(t_mlx_utils *, t_image_data, int, int);
+// 	t_set_select	*next_set;
+// }				t_set_select;
 
 
 ///////////////////////////***Prototipes***////////////////////////
+
+//SETS
+void		mandelbrot_set(t_mlx_utils *mlx, t_image_data *img);
+void		julia_set(t_mlx_utils *mlx, t_image_data *img);
+
+//SET_MLX_ENVIROMENT_TO_RENDER
+t_mlx_utils	*setup_mlx_enviroment(t_image_data **image);
+
+//SET IMAGE
+char		select_image_to_render(t_mlx_utils *mlx, char **argv, int argz);
+char		**load_args_table(int num_args, char **argv);
+
+//RENDER
+void		render_set(t_mlx_utils *mlx, t_image_data *image, int (*set_pixel)(t_mlx_utils *, t_image_data*, int, int));
+void		xy_to_image_coords(t_complex_factors *z, int x, int y);
+void		apply_zoom_and_moves(t_complex_factors	*z, int	x, int y);
+void		mlx_put_image_to_window(/**/);//cambiar mlx_data por mlx enviroment.
+
+//MATHS
+
+
+//MANAGE EVENTS
+
+
+//UTILS
+void		ft_error(char *message);
+double		atodbl(char *str);
 
 
 #endif
--- fractol/fractol/Src/main.c
+++ fractol/fractol/Src/main.c
@@ -6,63 +6,46 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 05:56:59 by alex              #+#    #+#             */
-/*   Updated: 2025/03/07 21:42:44 by alex             ###   ########.fr       */
+/*   Updated: 2025/03/27 01:27:27 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	main(int argc, char **argv)
+int	main(int argz, char **argv)
 {
-	t_mlx_utils		*mlx;
+	t_mlx_enviroment		*mlx;
 	t_image_data	*image;
-	int				(*f)[2];
 	
-	parse_argumets(image, (*f), 
-	// if (argc == 2)
-	// {
-	// 	if (ft_strncmp(argv[1], "mandelbrot", 10) && ft_strlen(argv[1]))
-	// 	{
-	// 		// do_mandelbrot();//TO DO;
-	// 		printf("mandelbrot\n");
-	// 	}
-	// 	else if (ft_strncmp(argv[1], "julia", 5) && ft_strlen(argv[1]))
-	// 	{
-	// 		// do_julia();//TO DO
-	// 		printf("julia\n");
-	// 	}
-	// 	else
-	// 	{
-	// 		printf("no valid argument name\n");
-	// 		return(1);
-	// 	}
-		if ((mlx = setup_mlx_enviroment(&image)) == NULL)
-			return (1)
-		render_fractol(mlx, image);
-		mlx_loop(mlx->mlx_data);
-
-		return (0);
-	// }
-
+	//parseo de limitees
+	if (argz < 2)
+		ft_error("Need more arguments\n");
+	//seleccionar imagen segun argumentos;
+	select_image_to_render(image, argv, argz);
+	//conectar con API, crear ventana y crear objeto imagen.
+	if ((mlx = setup_mlx_enviroment(&image)) == NULL)
+		return (1);
+	//renderizar el set que queramos representar
+	render_set(mlx, image, mandelbrot_set);
+	//mantener el programa en ejecucion hasta que evento (API X11) lo pare.
+	mlx_loop(mlx->mlx_var);
+	return (0);
 }
 
-
-t_mlx_utils	*setup_mlx_enviroment(t_image_data **image)
+t_mlx_enviroment	*setup_mlx_enviroment(t_image_data **image)
 {
-	t_mlx_utils		*mlx;
+	t_mlx_enviroment		*mlx;
 
-	mlx->mlx_data = mlx_init();
-	if (!mlx->mlx_data)
-	{
+	mlx->mlx_var = mlx_init();
+	if (!mlx->mlx_var)
 		return (NULL);
-	}
-	mlx->windows = mlx_new_window(mlx->mlx_data, mlx->win_data->height, mlx->win_data->height, "Fractol");
+	mlx->windows = mlx_new_window(mlx->mlx_var, mlx->win_data->height, mlx->win_data->height, "Fractol");
 	if (!mlx->windows)
 	{
 		//free_window 
 		return (NULL);
 	}
-	image[0]->img = mlx_new_image(mlx->mlx_data, HEIGHT, WIDTH);
+	image[0]->img = mlx_new_image(mlx->mlx_var, HEIGHT, WIDTH);
 	if (!image[0]->img)
 	{
 		//free_image and window
@@ -74,15 +57,72 @@ t_mlx_utils	*setup_mlx_enviroment(t_image_data **image)
 		//freee image and window
 		return (NULL);
 	}
+}
+
+char	select_image_to_render(t_mlx_enviroment *mlx, char **argv, int argz)
+{
+	if (ft_strncmp(argv[1], "mandelbrot", 10) && ft_strlen(argv[1]))
+	{
+		if (argz > 2)
+		{
+			ft_putstr_fd("Too many arguments for Mandelbrot\n", 2);
+			return (0);
+		}
+		mlx->img_data->set_def = mandelbrot_set;
+		mlx->img_data->config = load_args_table(argz - 2, argv);
+	}
+	else if (ft_strncmp(argv[1], "julia", 5) && ft_strlen(argv[1]))
+	{
+		if( /*argz == n*/)
+		{
+			ft_putstr_fd("ERROR, you need x arguments ofr Julia\n", 2);
+			return (0);
+		}
+		mlx->img_data->set_def = julia_set;
+		mlx->img_data->config = load_args_table(argz - 2, argv);
+	}
+	else
+	{
+		ft_putstr_fd("you have to choose Mandelbroor or Julia\n", 2);
+		return (0);
+	} 
+}
+
+void	mandelbrot_set(t_mlx_utils *mlx, t_image_data *img)
+{
 	
+}
+
+void	julia_set(t_mlx_utils *mlx, t_image_data *img)
+{
+	
+}
+
+char	**load_args_table(int num_args, char **argv)
+{
+	char	**table_args;
+	int		i;
+	
+	if (num_args == 0)
+		return (NULL);
+	table_args = (char **)malloc(sizeof(char *) * num_args);
+	if (!table_args)
+		return (NULL);
+	i = 1;
+	while (i < (num_args - 1) || argv[i] != NULL)
+	{
+		table_args[i] = argv[i];
+		i++;
+	}
+	return (table_args);
 }
 
 void	render_set(t_mlx_utils *mlx, t_image_data *image, int (*set_pixel)(t_mlx_utils *, t_image_data*, int, int))
 {
-	t_complex_data	*z;
-	int				pixel_color;
-	int 			x;
-	int 			y;
+	t_complex_factors	*z;
+	int					pixel_color;
+	int 				x;
+	int 				y;
 
 	x = -1 ;
 	while (++x < HEIGHT)
@@ -91,7 +131,7 @@ void	render_set(t_mlx_utils *mlx, t_image_data *image, int (*set_pixel)(t_mlx_ut
 		while (++y < WIDTH)
 		{
 			scale_xy_to_image_coords(&z, x, y);
-			apply_zoom_and_moves()
+			apply_zoom_and_moves(z, x, y);
 			pixel_color = set_pixel(mlx, image, z->a, z->b);
 			buffer_my_pixel(z->a, z->b, image->bit_map_address, pixel_color);
 		}
@@ -99,22 +139,31 @@ void	render_set(t_mlx_utils *mlx, t_image_data *image, int (*set_pixel)(t_mlx_ut
 	mlx_put_image_to_window(mlx->mlx_data, mlx->windows, image, x, y);//cambiar mlx_data por mlx enviroment.
 }
 
-void	xy_to_image_coords(t_complex_data *z, int x, int y)
+void	xy_to_image_coords(t_complex_factors *z, int x, int y)
 {
 	double	origin_x;
 	double	origin_y;
 	
 	origin_x = (WIDTH / 2);
 	origin_y = (HEIGHT / 2);
+	//los mivimeintos sobre la pantalla los voy a tener que meter antes de aplicar el algoritmo y el escalado
 	z->a = (x - origin_x) * ((MAND_MAX_X - MAND_MIN_X) / (WIDTH - 0));
 	z->b = (y - origin_y) * ((MAND_MAX_Y - MAND_MIN_Y) / (HEIGHT - 0));
+	//o los meto despues del escalado conuna relacion
 }
 
-int	fmandelbroot_set(t_mlx_utils *mlx, t_image_data *imag, double a, double b)
+void		apply_zoom_and_moves(t_complex_factors	*z, int	x, int y)
 {
-	// if (!imag->config)
-	// {
-	// 	return (0);
-	// }
 	
 }
+
+//void mlx_put_image_to_window(mlx->mlx_data, mlx->windows, image, x, y);//cambiar mlx_data por mlx enviroment.
+
+void	ft_error(char *message)
+{
+	if (!message)
+		return ;
+	ft_putstr_fd(message, 2);
+	return (1);	
+}
+
